44dc9103dbc65b86699d06932a55ed11199c5a7c
Pablo <juanpa92008@gmail.com>
2020-05-10 03:48:04 -0500

    React router

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () =>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna ante, porttitor sed turpis vitae, vehicula pulvinar leo. Praesent eleifend efficitur nisl vel vulputate. Maecenas a bibendum diam, a vestibulum tortor. Pellentesque metus eros, volutpat in hendrerit a, pharetra eu eros. Sed ullamcorper nec est id congue.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () =>{
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna ante, porttitor sed turpis vitae, vehicula pulvinar leo. Praesent eleifend efficitur nisl vel vulputate. Maecenas a bibendum diam, a vestibulum tortor. Pellentesque metus eros, volutpat in hendrerit a, pharetra eu eros. Sed ullamcorper nec est id congue.</p>
+        </div>
+    )
+}
+
+export default Home
