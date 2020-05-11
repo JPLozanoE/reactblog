a559e2c346f1c234ec7e9ccc3493b6eb347b40c6
Pablo <juanpa92008@gmail.com>
2020-05-10 19:21:57 -0500

    Manejo de imagenes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import ReactLogo from '../../logo192.png'
 
 class Home extends React.Component{
     state={
@@ -21,9 +22,10 @@ class Home extends React.Component{
             posts.map(post=>{
                 return(
                     <div className="post card" key={post.id}>
+                        <img src={ReactLogo} alt="A React logoo"/>
                         <div className="card-content">
                         <Link to={'/'+post.id}>
-                            <span className="card-title">{post.title}</span>
+                            <span className="card-title blue-text">{post.title}</span>
                         </Link>
                             <p>{post.body}</p>
                         </div>
@@ -34,7 +36,7 @@ class Home extends React.Component{
             <div className="center">No posts yet</div>
         )
         return(
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
